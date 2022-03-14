0f1a30cbdaff53063d046d9b75291503fc6a2104
Savanah Trewman <strewman@me.com>
2022-03-14 17:18:30 -0500

    changed home page posts

--- react-app/src/components/Posts/AllPosts/PostDetail.js
+++ react-app/src/components/Posts/AllPosts/PostDetail.js
@@ -10,19 +10,43 @@ const PostDetail = ({ post }) => {
 
     // const username = useParams();
 
+    // Need to make a check, created_at VS updated_at
+        // Posted by u/demo 12 hours ago
+        // * Edited by u/demo 12 hours ago
+
+    // const getToday = () => {
+    //     const today = new Date();
+
+    //     const yyyy = today.getFullYear();
+    //     const mm = (today.getMonth() + 1).toString().padStart(2, "0");
+    //     const dd = (today.getDate()).toString().padStart(2, "0");
+    //     const hh = (today.getHours()).toString().padStart(2, "0");
+    //     const min = (today.getMinutes()).toString().padStart(2, "0");
+
+    //     return yyyy + "-" + mm + "-" + dd + ' ' + hh + ':' + min;
+    // };
+
+    const dateFormatted = (date) => {
+        const dateSplit = date.split(' ');
+        const newDate = dateSplit[2] + '-' + dateSplit[1] + '-' + dateSplit[3];
+        return newDate;
+    }
+
+    console.log('tiiiiiiime', dateFormatted(post.created_at))
+
     return (
         <div className='post-detail-container'>
-            <NavLink to={`/posts/${post.id}`} className='post-navlink-container'>
-                <div id='post-username'>
-                    {post.username}:
-                </div>
-                <div id='post-title'>
-                    {post.title}
-                </div>
-                <div id='post-caption'>
-                    {post.caption}
-                </div>
-            </NavLink>
+            {/* <NavLink to={`/posts/${post.id}`} className='post-navlink-container'>
+            </NavLink> */}
+            <div id='post-username'>
+                Posted by u/{post.username} on {dateFormatted(post.created_at)}
+            </div>
+            <div id='post-title'>
+                {post.title}
+            </div>
+            <div id='post-caption'>
+                {post.caption}
+            </div>
         </div>
     )
 }
