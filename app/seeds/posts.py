aa8086daa85e901e0ac0346f7f34250797a67a30
Joey Marino <joeymarino94@gmail.com>
2022-03-14 14:14:26 -0500

    created more seeds

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -6,10 +6,16 @@ def seed_posts():
     post1 = Post(user_id=1, title='test1', caption='This is the caption to the title TEST 1')
     post2 = Post(user_id=2, title='test2', caption='If you can see this, then the post is working really well')
     post3 = Post(user_id=3, title='test3', caption='This all but confirms that the functionality is there!!! hooray!')
+    post4 = Post(user_id=4, title='test4', caption='This is the caption to the title TEST 4')
+    post5 = Post(user_id=5, title='test5', caption='This is the caption to the title TEST 5')
+    post6 = Post(user_id=6, title='test6', caption='This is the caption to the title TEST 6')
 
     db.session.add(post1)
     db.session.add(post2)
     db.session.add(post3)
+    db.session.add(post4)
+    db.session.add(post5)
+    db.session.add(post6)
 
     db.session.commit()
 
--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,10 +6,18 @@ def seed_users():
     demo = User(username='demo', address=123, password='password')
     joey = User(username='joey', address=1234, password='password')
     savanah = User(username='savanah', address=12345, password='password')
+    satoshi = User(username='satoshi', address=123456, password='password')
+    vitalik = User(username='vitalik', address=1234567, password='password')
+    gavin = User(username='gavin', address=12345678, password='password')
+    charles = User(username='charles', address=123456789, password='password')
 
     db.session.add(demo)
     db.session.add(joey)
     db.session.add(savanah)
+    db.session.add(satoshi)
+    db.session.add(vitalik)
+    db.session.add(gavin)
+    db.session.add(charles)
 
     db.session.commit()
 
