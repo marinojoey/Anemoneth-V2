0e010540244f3f55724cfc9e4f942cb31d1150f9
Joey Marino <joeymarino94@gmail.com>
2022-03-14 19:56:36 -0500

    created redeemButton files

---
+++ react-app/src/components/RedeemButton/RedeemButton.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import contractCall from '../ContractCall/ContractCall'
+import { ethers } from "ethers";
+import './redeemButtons.css'
+const { ethereum } = window;
+
+
+function RedeemButton() {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default RedeemButton
